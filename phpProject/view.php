25a169e9ef1d7e43856eeb8ee69b29f70ecb2631
VasilikiLoukoumi <loukoumi.vas@gmail.com>
2017-09-26 23:52:23 +0300

    Update view.php

--- phpProject/view.php
+++ phpProject/view.php
@@ -6,10 +6,10 @@ require 'connect.php';
 function show(){
 $result = mysql_query("SELECT * FROM `data`");
 //fetch data 
-$mydata = "<table><thead><th>Name</th><th>Staring</th><th>Release year</th><th>Short Review</th></thead><tbody>";
+$mydata = "<table><thead><th>Name</th><th>Starring</th><th>Release year</th><th>Short Review</th></thead><tbody>";
 
 while ($row = mysql_fetch_array($result)) {
-   $mydata.= "<tr><td>".$row{'name'}."</td><td>".$row{'staring'}."</td><td>
+   $mydata.= "<tr><td>".$row{'name'}."</td><td>".$row{'starring'}."</td><td>
    ".$row{'release_year'}." (*Greek theaters)</td><td>".$row{'review'}."</td></tr>";
 }
 $mydata.="</tbody></table>";
